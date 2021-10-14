2038c41d78491c14e50affde523ddbf410ca647b
John Doe <johndoe@example.com>
2021-10-14 16:40:04 +0600

    update

--- MID_TASK_FILE_CURD/userlist.php
+++ MID_TASK_FILE_CURD/userlist.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>User List</title>
 </head>
+
 <body>
-	<center>	
+
+	<center>
 		<a href="home.php">Back </a> |
-		<a href="logout.php">logout </a> 
+		<a href="logout.php">logout </a>
 	</center>
+
 	<br>
+
 	<table border="1" align="center">
 		<tr>
 			<th>ID</th>
@@ -18,26 +23,30 @@
 			<th>EMAIL</th>
 			<th>ACTION</th>
 		</tr>
-		<tr>
-			<td>1</td>
-			<td>alamin</td>
-			<td>12</td>
-			<td>alamin@aiub.edu</td>
-			<td>
-				<a href="edit.php?id=1"> EDIT</a> | 
-				<a href="delete.php?id=1"> DELETE</a> 
-			</td>
-		</tr>
-		<tr>
-			<td>1</td>
-			<td>alamin</td>
-			<td>12</td>
-			<td>alamin@aiub.edu</td>
-			<td>
-				<a href="edit.php?id=2"> EDIT</a> | 
-				<a href="delete.php?id=2"> DELETE</a> 
-			</td>
-		</tr>
+		<?php
+		$myfile = fopen('user.txt', 'r');
+		$counter = 0;
+
+		while (!feof($myfile)) {
+			$data = fgets($myfile);
+			if($data!=""){
+				$user = explode('|', $data);
+	
+				echo '<tr>
+					<td>' . ++$counter . '</td>
+					<td>' . $user[0] . '</td>
+					<td>' . $user[1] . '</td>
+					<td>' . $user[2] . '</td>
+					<td>
+						<a href="edit.php?id='.$counter.'"> EDIT</a> |
+						<a href="delete.php?id='.$counter.'"> DELETE</a>
+					</td>
+				</tr>';
+			}
+		}
+		fclose($myfile);
+		?>
 	</table>
 </body>
+
 </html>
