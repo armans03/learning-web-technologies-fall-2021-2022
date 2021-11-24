bec5907c8592d21b6e22f03caf61018cfe1b1384
John Doe <johndoe@example.com>
2021-11-24 15:25:09 +0600

    1st commit

---
+++ FINAL_LAB_PERFORMANCE_2/handler.php
@@ -0,0 +1,46 @@
+<?php
+
+$no1 = $_POST['no1'] ;
+$no2 = $_POST['no2'] ;
+$op = $_POST['operand'];
+
+
+switch ($op) {
+	case '-':
+		$res = $no1-$no2;
+        echo $res;
+		break;
+	case '*':
+		$res = $no1*$no2;
+        echo $res;
+		break;
+	case '/':
+		$res = $no1/$no2;
+        echo $res;
+		break;
+	case '+':
+		$res = $no1+$no2;
+		echo $res;
+		break;
+
+}
+
+
+/*else if ($op == '-') {
+	$res = $no1-$no2;
+
+	echo $res;
+}
+else if ($op == '*') {
+	$res = $no1*$no2;
+
+	echo $res;
+}
+else if ($op == '/') {
+	$res = $no1/$no2;
+
+	echo $res;
+}*/
+
+
+?>
